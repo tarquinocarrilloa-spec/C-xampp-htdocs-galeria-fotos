ac5bc5bace193c710841069b2883e8aa1688628d
alex <tarquinocarrilloa@gmail.com>
2026-09-01 18:45:20 -0400

    agregando archivos del proyecto

---
+++ config/global.php
@@ -0,0 +1,11 @@
+<?php
+// Configuración de la Base de Datos
+define("DB_HOST", "localhost");
+define("DB_NAME", "galeria_fotos");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_ENCODE", "utf8");
+
+// Definición de la constante de la URL base del proyecto
+define("PRO_NOMBRE", "Galería de Fotos");
+?>
